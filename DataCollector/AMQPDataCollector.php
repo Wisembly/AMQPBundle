1e334e7b9d0be8fcb68e25026ccb5c5c9c465410
Baptiste Clavié <baptiste@wisembly.com>
2014-12-12 18:57:15 +0100

    Move DataCollector amqp related stuff into the amqp bundle

---
+++ DataCollector/AMQPDataCollector.php
@@ -0,0 +1,57 @@
+<?php
+namespace Wisembly\AmqpBundle\DataCollector;
+
+use Exception;
+
+use Symfony\Component\HttpFoundation\Request,
+    Symfony\Component\HttpFoundation\Response,
+
+    Symfony\Component\HttpKernel\DataCollector\DataCollector;
+
+/**
+ * Collect all the data when sending a message through our publisher
+ *
+ * @author Baptiste Clavié <baptiste@wisembly.com>
+ */
+class AMQPDataCollector extends DataCollector
+{
+    private $messages = [];
+
+    public function __construct()
+    {
+        $this->data = ['count' => 0,
+                       'messages' => []];
+    }
+
+    public function collect(Request $request, Response $response, Exception $exception = null)
+    {
+        $this->data = ['messages' => $this->messages,
+                       'count'    => count($this->messages)];
+    }
+
+    /**
+     * Add some data to the collector
+     *
+     * @param string[] $data message to collect
+     */
+    public function addMessage(array $data)
+    {
+        $this->messages[] = $data;
+    }
+
+    public function getMessages()
+    {
+        return $this->data['messages'];
+    }
+
+    public function getCount()
+    {
+        return $this->data['count'];
+    }
+
+    public function getName()
+    {
+        return 'amqp_collector';
+    }
+}
+
