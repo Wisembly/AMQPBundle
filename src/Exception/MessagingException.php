1b6f0e760f88bf538e792066d156d08921d748fc
Baptiste Clavié <clavie.b@gmail.com>
2017-12-13 10:50:21 +0100

    Remove old MessagingException

--- src/Exception/MessagingException.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Wisembly\AmqpBundle\Exception;
-
-use RuntimeException;
-
-class MessagingException extends RuntimeException
-{
-}
--- src/MessagingException.php
+++ src/MessagingException.php
@@ -2,22 +2,12 @@
 namespace Wisembly\AmqpBundle;
 
 use Throwable;
-//use RuntimeException; // to uncomment when bc is gone
+use RuntimeException;
 
-use Wisembly\AmqpBundle\Exception\MessagingException as BC;
-
-final class MessagingException extends BC
+final class MessagingException extends RuntimeException
 {
     public function __construct(Throwable $t)
     {
         parent::__construct('There was an error while trying to use the Messaging service', $t->getCode(), $t);
-    }
-
-    /** @deprecated Use $e->getPrevious()->getMessage() instead... */
-    public function getMessagingExceptionMessage(): string
-    {
-        @trigger_error(sprintf('The method %s is deprecated since 1.4.0, please use getPrevious()->getMessage() instead', __CLASS__, __METHOD__), \E_USER_DEPRECATED);
-
-        return $this->getPrevious()->getMessage();
     }
 }
